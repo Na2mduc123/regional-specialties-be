1dedc02fb94584e8281150d42f168184ccbf87a1
Na2mduc123 <vutunam03@gmail.com>
2025-10-13 11:02:41 +0700

    try 10s token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -245,7 +245,7 @@ export const login = async (req: Request, res: Response) => {
     const token = jwt.sign(
       { id: String(user.id), username: user.username, role: user.role },
       JWT_SECRET,
-      { expiresIn: "1h" }
+      { expiresIn: "10s" }
     );
 
     res.json({
